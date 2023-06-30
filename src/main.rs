bece6df7fa44d892860a5dea77e2dc37efcd77b5
Zeeshan Ali Khan <zeeshanak@gnome.org>
2023-06-30 16:35:09 +0200

    ✨ Support repos with only 1 project

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use regex::Regex;
 
 fn main() {
     let args: Vec<String> = args().collect();
-    if args.len() < 3 {
+    if args.len() < 2 {
         println!("Usage: {} REPO_DIR SUB_PROJECT", args[0]);
 
         return;
     }
     let local_repo = &args[1];
-    let subproject = &args[2];
-    let subproject_prefix = format!("{}-", subproject);
+    let subproject = args.get(2);
+    let subproject_prefix = subproject.map(|s| format!("{}-", s));
 
     // Get the tags from the local repo for the subproject
     let tags_list_cmd_out = Command::new("git")
@@ -23,22 +23,28 @@ fn main() {
     let tags_list = String::from_utf8(tags_list_cmd_out).unwrap();
     let mut tags_list: Vec<&str> = tags_list
         .split_whitespace()
-        .filter(|tag| tag.starts_with(&subproject_prefix))
+        .filter(|tag| match &subproject_prefix {
+            Some(prefix) => tag.starts_with(prefix),
+            None => true,
+        })
         .collect();
     tags_list.reverse();
     let last_release = tags_list[0];
     let range = format!("{}..HEAD", last_release);
 
     // list all commit hashes since the last release
-    let git_log_cmd_out = Command::new("git")
-        .args(&[
-            "log",
-            "--no-color",
-            // <subject> <dot> <space> <body> END
-            "--pretty=format:%s. %bEND",
-            &range,
-            &subproject,
-        ])
+    let mut git_log_cmd_out = Command::new("git");
+    git_log_cmd_out.args(&[
+        "log",
+        "--no-color",
+        // <subject> <dot> <space> <body> END
+        "--pretty=format:%s. %bEND",
+        &range,
+    ]);
+    if let Some(subproject) = subproject {
+        git_log_cmd_out.arg(subproject);
+    }
+    let git_log_cmd_out = git_log_cmd_out
         .current_dir(local_repo)
         .output()
         .expect("failed to execute process")
